a1e89b44ce80dd17fd2f92b04174bee4f1ead297
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-27 12:21:26 +0100

    Chapter 19: the single runtime chunk

--- assets/js/admin_article_form.js
+++ assets/js/admin_article_form.js
@@ -48,6 +48,9 @@ $(document).ready(function() {
 class ReferenceList
 {
     constructor($element) {
+
+        var stuff = new WeakSet([]);
+
         this.$element = $element;
         this.sortable = Sortable.create(this.$element[0], {
             handle: '.drag-handle',
--- assets/js/article_show.js
+++ assets/js/article_show.js
@@ -1,7 +1,10 @@
-import '../styles/article_show.scss'
-import $ from 'jquery'
+import '../styles/article_show.scss';
+import $ from 'jquery';
+//import 'bootstrap';
 
 $(document).ready(function() {
+    $('.js-like-article').tooltip();
+
     $('.js-like-article').on('click', function(e) {
         e.preventDefault();
 
