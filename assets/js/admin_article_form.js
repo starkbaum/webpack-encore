d6f00b16763dfedece016dfee4073131406cd779
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-27 11:29:57 +0100

    Chapter 14: auto-provide jquery for mischievous packages

--- assets/js/admin_article_form.js
+++ assets/js/admin_article_form.js
@@ -2,12 +2,16 @@ import $ from 'jquery';
 import Dropzone from 'dropzone';
 import 'dropzone/dist/dropzone.css';
 import Sortable from 'sortablejs';
-import './components/algolia-autocomplete';
-import '../styles/algolia-autocomplete.scss';
+import autocomplete from './components/algolia-autocomplete';
 
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
+    const $autocomplete = $('.js-user-autocomplete');
+    if (!$autocomplete.is(':disabled')) {
+        autocomplete($autocomplete, 'users', 'email');
+    }
+
     const $referenceList = $('.js-reference-list');
     if ($referenceList[0]) {
         var referenceList = new ReferenceList($('.js-reference-list'));
