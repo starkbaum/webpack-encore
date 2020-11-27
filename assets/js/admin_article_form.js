3edbae5319de995d185af41f2419acd34b70cfb1
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-27 12:28:51 +0100

    Chapter 20: async imports

--- assets/js/admin_article_form.js
+++ assets/js/admin_article_form.js
@@ -2,14 +2,15 @@ import $ from 'jquery';
 import Dropzone from 'dropzone';
 import 'dropzone/dist/dropzone.css';
 import Sortable from 'sortablejs';
-import autocomplete from './components/algolia-autocomplete';
 
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
     const $autocomplete = $('.js-user-autocomplete');
     if (!$autocomplete.is(':disabled')) {
-        autocomplete($autocomplete, 'users', 'email');
+        import('./components/algolia-autocomplete').then((autocomplete => {
+            autocomplete.default($autocomplete, 'users', 'email');
+        }));
     }
 
     const $referenceList = $('.js-reference-list');
